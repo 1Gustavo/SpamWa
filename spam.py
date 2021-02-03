046f8a6474d62a2489b0abd02d34e639daf1d02c
NUKE MODS <76784687+1Gustavo@users.noreply.github.com>
2021-02-03 10:18:00 -0300

    Update spam.py

--- spam.py
+++ spam.py
@@ -209,19 +209,14 @@ def multi():
 #-------------------------Fungsi Banner-----------------------
 def logo():
 	os.system('clear')
-	auth=m+'  Author : '+k+'./kitsune'
+	auth=m+'  Author : '+k+'|NUKEMODS|'
 	# jika ingin m3namambah kan variabel dan mengubah data variabel kitsune bisa menambahkan %s menambahkan variabel terus di ubah menjjadu string, %d = mengubah data menjadi decimal , %i = mengubah data menjadi integer
 	return '''
-%s╭━┳━╭━╭━╮%s╮╲╲╲╲╲╲%s╔═╗╔═╗╔═╗╔╦╗
-%s┃┈┈┈┣▅╋▅┫┃%s╲╲╲╲╲╲%s╚═╗╠═╝╠═╣║║║
-%s┃┈┃┈╰━╰━━━━━━╮%s╲╲%s╚═╝╩  ╩ ╩╩ ╩
-%s╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣%s╲%s╔═╗╔╦╗╔═╗
-%s╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉%s╲%s╚═╗║║║╚═╗
-%s╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤%s╲%s╚═╝╩ ╩╚═╝
-%s╲┃┈┈┈┈╭━┳━━━━╯%s╲╲%s╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗
-%s╲┣━━━━━━┫%s╲╲╲╲╲╲╲%s║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
-%s╲┃┈┈┈┈┈┈┃%s╲╲╲╲╲╲╲%s╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩  
-%s''' % (k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,auth)
+╔═╦╗╔╦╗╔╦╗╔═╗  ╔══╗╔═╗╔══╗╔═╦═╗  ╔╦═╦╗╔══╗
+║║║║║║║║╔╝║╦╝  ║══╣║╬║║╔╗║║║║║║  ║║║║║║╔╗║
+║║║║║║║║╚╗║╩╗  ╠══║║╔╝║╠╣║║║║║║  ║║║║║║╠╣║
+╚╩═╝╚═╝╚╩╝╚═╝  ╚══╝╚╝─╚╝╚╝╚╩═╩╝  ╚═╩═╝╚╝╚╝
+─────────────  ────────────────  ─────────
 # -----------------------------------------------------------
 def termux():
 	os.system('termux-contact-list > .contact')
